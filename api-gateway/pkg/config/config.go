dc9659f58c20dbd58841dbf29787f86c9e9a40bc
DOO-DEV <nimo.vimo.yank@gmail.com>
2023-09-05 21:43:36 +0330

    build: add env

---
+++ api-gateway/pkg/config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	Port          string `mapstructure:"PORT"`
+	AuthSvcUrl    string `mapstructure:"AUTH_SVC_URL"`
+	ProductSvcUrl string `mapstructure:"PRODUCT_SVC_URL"`
+	OrderSvcUrl   string `mapstructure:"ORDER_SVC_URL"`
+}
+
+func LoadConfig() (Config, error) {
+	c := Config{}
+
+	viper.AddConfigPath("./pkg/config/envs")
+	viper.SetConfigName("dev")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		return Config{}, err
+	}
+
+	if err := viper.Unmarshal(&c); err != nil {
+		return Config{}, err
+	}
+
+	return c, nil
+}
