2de0e22a37f21cbf92935c585ea0ad0b196969ec
DOO-DEV <nimo.vimo.yank@gmail.com>
2023-09-06 16:31:39 +0330

    feat: implement product svc

---
+++ product-svc/cmd/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"product-svc/pkg/config"
+	"product-svc/pkg/db"
+	"product-svc/pkg/pb"
+	"product-svc/pkg/services"
+)
+
+func main() {
+	c, err := config.LoadConfig()
+
+	if err != nil {
+		log.Fatalln("Failed at config", err)
+	}
+
+	h := db.Init(c.DBUrl)
+
+	lis, err := net.Listen("tcp", c.Port)
+
+	if err != nil {
+		log.Fatalln("Failed to listing:", err)
+	}
+
+	fmt.Println("Product Svc on", c.Port)
+
+	s := services.Server{
+		H: h,
+	}
+
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterProductServiceServer(grpcServer, &s)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalln("Failed to serve:", err)
+	}
+}
